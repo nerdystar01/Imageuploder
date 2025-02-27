efdea8d8bcefdb2df240d1646f2b4002b6677467
nerdystar <nerdystar@nerdystarui-MacBookPro.local>
2025-02-27 10:47:16 +0900

    [2/27][MOD]: 모델링 수정 2

---
+++ bulk-upload-v3/manager.py
@@ -0,0 +1,470 @@
+# TAG_MAPPING = {
+#    "andrew,": "andrew",
+#    "Akane,": "Akane",
+#    "4g9_beat,": "Beat",
+#    "beat,": "Beat",
+#    "blake,": "blake",
+#    "celmech,": "celmech",
+#    "gris,": "gris",
+#    "hyde,": "hyde",
+#    "hydo,": "Hyde",
+#    "4g9_cooper,": "JohnCooper",
+#    "cooper,": "JohnCooper",
+#    "moonstone,": "moonstone",
+#    "muro,": "muro",
+#    "muro_mech,": "muro_mech",
+#    "oscar,": "oscar",
+#    "P_5.D,": "P-5",
+#    "ravi,": "ravi",
+#    "rv,": "rv",
+#    "slasher,": "slasher",
+#    "yugo,": "UGo",
+#    "sol,": "Soll",
+#    "soll,": "Soll",
+#    "verdoman,": "Verdorman",
+#    "engel,": "engel",
+#    "4g9_honey,": "Hani",
+#    "sniper_girl,": "Hani",
+#    "joa,": "Joa",
+#    "4g9_joa,": "Joa",
+#    "lora,": "Lora",
+#    "4g9_lora,": "Lora",
+#    "mother,": "Mother",
+#    "4g9_mother,": "Mother",
+#    "pre,": "Pre",
+#    "4g9_pre,": "Pre",
+#    "4g9_scarlet,": "Scarlett",
+#    "4g9_zp,": "ZP",
+#    "agro,": "agro",
+#    "aguro,": "agro",
+#    "baeksa,": "baeksa",
+#    "beaksa,": "Baeksa",
+#    "blanc,": "blanc",
+#    "chati,": "chati",
+#    "experiment01,": "experiment01",
+#    "experiment02,": "experiment02",
+#    "employee,": "employee",
+#    "hayden,": "hayden",
+#    "hikey_female,": "Hayden",
+#    "heize,": "heize",
+#    "singer,": "Heize",
+#    "singer_female,": "Heize",
+#    "singer_male,": "Oscar",
+#    "hikey,": "Hyki",
+#    "hyki,": "hyki",
+#    "irie,": "Irey",
+#    "lettergirl,": "lettergirl",
+#    "mari,": "mari",
+#    "milter,": "milter",
+#    "milter_female,": "Milter",
+#    "navy_girl,": "navy_girl",
+#    "nino,": "nino",
+#    "noah,": "noah",
+#    "persa,": "persa",
+#    "fersa,": "Persa",
+#    "father,": "father",
+#    "rem,": "rem",
+#    "rinmei,": "LinMei",
+#    "rinshao,": "LinXiao",
+#    "rinshao_female,": "LinXiao",
+#    "ru-i,": "RU-i",
+#    "santos,": "santos",
+#    "santos_female,": "Santos",
+#    "shimizu,": "shimizu",
+#    "space_girl,": "Danbi",
+#    "space girl,": "Danbi",
+#    "armor,": "armor",
+#    "blouse,": "blouse",
+#    "buruma,": "buruma",
+#    "shirt,": "shirt",
+#    "t-shirt,": "t-shirt",
+#    "cardigan,": "cardigan",
+#    "camisole,": "camisole",
+#    "cape,": "cape",
+#    "cassock,": "cassock",
+#    "coat,": "coat",
+#    "fur coat,": "fur_coat",
+#    "fur_coat,": "fur_coat",
+#    "raincoat,": "raincoat",
+#    "jacket,": "jacket",
+#    "blazer,": "blazer",
+#    "business suit,": "business_suit",
+#    "business_suit,": "business_suit",
+#    "chinese clothes,": "chinese_clothes",
+#    "chinese_clothes,": "chinese_clothes",
+#    "male swimwear,": "male_swimwear",
+#    "male_swimwear,": "male_swimwear",
+#    "band uniform,": "band_uniform",
+#    "band_uniform,": "band_uniform",
+#    "school uniform,": "school_uniform",
+#    "school_uniform,": "school_uniform",
+#    "sportswear,": "sportswear",
+#    "tank top,": "tank_top",
+#    "tank_top,": "tank_top",
+#    "gym uniform,": "gym_uniform",
+#    "gym_uniform,": "gym_uniform",
+#    "sweater,": "sweater",
+#    "crop top,": "crop_top",
+#    "crop_top,": "crop_top",
+#    "tuxedo,": "tuxedo",
+#    "winter clothes,": "winter_clothes",
+#    "winter_clothes,": "winter_clothes",
+#    "military uniform,": "military_uniform",
+#    "military," : "military",
+#    "military_uniform,": "military_uniform",
+#    "apron,": "apron",
+#    "maid,": "maid",
+#    "cheerleader,": "cheerleader",
+#    "nun,": "nun",
+#    "overalls,": "overalls",
+#    "pajamas,": "pajamas",
+#    "police,": "police",
+#    "skirt,": "skirt",
+#    "long skirt,": "long_skirt",
+#    "long_skirt,": "long_skirt",
+#    "shorts,": "shorts",
+#    "pants,": "pants",
+#    "fashion,": "fashion",
+#    "casual,": "casual",
+#    "dress,": "dress",
+#    "wedding,": "wedding",
+#    "mechanical,": "mechanical",
+#    "bikini,": "bikini",
+#    "swimsuit,": "swimsuit",
+#    "ballet,": "ballet",
+#    "robe,": "robe",
+#    "japanese clothes,": "japanese_clothes",
+#    "japanese_clothes,": "japanese_clothes",
+#    "korean clothes,": "korean_clothes",
+#    "korean_clothes,": "korean_clothes",
+#    "animal costume,": "animal_costume",
+#    "animal_costume,": "animal_costume",
+#    "santa costume,": "santa_costume",
+#    "santa_costume,": "santa_costume",
+#    "bodysuit,": "bodysuit",
+#    "bikesuit,": "bikesuit",
+#    "jumpsuit,": "jumpsuit",
+#    "leotard,": "leotard",
+#    "nightgown,": "nightgown",
+#    "kneehighs,": "kneehighs",
+#    "pantyhose,": "pantyhose",
+#    "thighhighs,": "thighhighs",
+#    "shoes,": "shoes",
+#    "boots,": "boots",
+#    "high heels,": "high_heels",
+#    "high_heels,": "high_heels",
+#    "sandals,": "sandals",
+#    "sneakers,": "sneakers",
+#    "headphones,": "headphones",
+#    "mask,": "mask",
+#    "necktie,": "necktie",
+#    "weapon,": "weapon",
+#    "simple background,": "simple_background",
+#    "simple_background,": "simple_background",
+#    "1girl,": "girl",
+#    "1boy,": "boy",
+#    "2boys,": "multiple",
+#    "2girls,": "multiple",
+#    "train,": "train",
+#    "street,": "street",
+#    "park,": "park",
+#    "outdoors,": "outdoors",
+#    "ocean,": "ocean",
+#    "indoors,": "indoors",
+#    "cyberpunk,": "cyberpunk",
+#    "city,": "city",
+#    "alley,": "alley",
+#    "autumn,": "autumn",
+#    "christmas,": "christmas",
+#    "flower,": "flower",
+#    "halloween,": "halloween",
+#    "new-year,": "new-year",
+#    "swimming,": "swimming",
+#    "thanksgiving-day,": "thanksgiving-day",
+#    "valentine,": "valentine",
+#    "winter-festival,": "winter-festival",
+#    "winter-clothes," : "winter-clothes",
+#    "winter clothes," : "winter-clothes"
+# }
+
+
+class BaseItem:
+    def __init__(self, name, aliases):
+        self.name = name
+        self.aliases = aliases
+
+class BaseManager:
+    def __init__(self):
+        self.items = {}
+    
+    def get_standard_name(self, alias):
+        """별칭으로 표준 이름을 찾습니다."""
+        for item in self.items.values():
+            if alias in item.aliases:
+                return item.name
+        return None
+    
+    def get_item(self, name):
+        """이름으로 아이템 객체를 반환합니다."""
+        return self.items.get(name)
+    
+    def get_all_aliases(self, name):
+        """아이템의 모든 별칭을 반환합니다."""
+        item = self.items.get(name)
+        return item.aliases if item else None
+    
+    def add_alias(self, name, new_alias):
+        """새로운 별칭을 추가합니다."""
+        item = self.items.get(name)
+        if item:
+            item.aliases.append(new_alias)
+            return True
+        return False
+
+class CharacterManager(BaseManager):
+    def __init__(self):
+        super().__init__()
+        self.items = {
+            # 앤드류
+            'Andrew': BaseItem('Andrew', ['andrew,']),
+            # 아카네
+            'Akane': BaseItem('Akane', ['Akane,', 'akane,']),
+            # 비트
+            'Beat': BaseItem('Beat', ['4g9_beat,', 'beat,']),
+            # 블레이크
+            'Blake': BaseItem('Blake', ['blake,']),
+            # 셀막
+            'Celmech': BaseItem('Celmech', ['celmech,']),
+            # 그리스
+            'Gris': BaseItem('Gris', ['gris,']),
+            # 하이도
+            'Hyde': BaseItem('Hyde', ['hyde,', 'hydo,']),
+            # 존쿠퍼
+            'JohnCooper': BaseItem('JohnCooper', ['4g9_cooper,', 'cooper,', 'johncooper']),
+            # 문스톤
+            'Moonstone': BaseItem('Moonstone', ['moonstone,']),
+            # 무로
+            'Muro': BaseItem('Muro', ['muro,', 'muro_mech,']),
+            # 오스카
+            'Oscar': BaseItem('Oscar', ['oscar,', 'singer_male,']),
+            # P-5
+            'P-5': BaseItem('P-5', ['P_5.D,']),
+            # 라비
+            'Ravi': BaseItem('Ravi', ['ravi,']),
+            # RV
+            'Rv': BaseItem('Rv', ['rv,']),
+            # 슬레셔
+            'Slasher': BaseItem('Slasher', ['slasher,']),
+            # 유고
+            'UGo': BaseItem('UGo', ['yugo,']),
+            # 솔
+            'Soll': BaseItem('Soll', ['sol,', 'soll,']),
+            # 베르도르만
+            'Verdorman': BaseItem('Verdorman', ['verdoman,', 'verdorman.']),
+            # 엔젤
+            'Engel': BaseItem('Engel', ['engel,']),
+            # 하니
+            'Hani': BaseItem('Hani', ['4g9_honey,', 'sniper_girl,']),
+            # 조아
+            'Joa': BaseItem('Joa', ['joa,', '4g9_joa,']),
+            # 로라
+            'Lora': BaseItem('Lora', ['lora,', '4g9_lora,']),
+            # 마더
+            'Mother': BaseItem('Mother', ['mother,', '4g9_mother,']),
+            # 프리
+            'Pre': BaseItem('Pre', ['pre,', '4g9_pre,']),
+            # 스칼렛
+            'Scarlett': BaseItem('Scarlett', ['4g9_scarlet,', '4g9_scarlett,']),
+            # ZP
+            'ZP': BaseItem('ZP', ['4g9_zp,', 'zp,', 'ZP']),
+            # 이고르
+            'Agro': BaseItem('Agro', ['agro,', 'aguro,']),
+            # 백사
+            'Baeksa': BaseItem('Baeksa', ['baeksa,', 'beaksa,']),
+            # 블랑
+            'Blanc': BaseItem('Blanc', ['blanc,']),
+            # 채티
+            'Chati': BaseItem('Chati', ['chati,']),
+            # 실험체 01
+            'Experiment01': BaseItem('Experiment01', ['experiment01,']),
+            # 실험체 02
+            'Experiment02': BaseItem('Experiment02', ['experiment02,']),
+            # 사장
+            'Employee': BaseItem('Employee', ['employee,']),
+            # 하이든
+            'Hayden': BaseItem('Hayden', ['hayden,', 'hikey_female,']),
+            # 헤이즈
+            'Heize': BaseItem('Heize', ['heize,', 'singer,', 'singer_female,']),
+            # 하이키
+            'Hyki': BaseItem('Hyki', ['hikey,', 'hyki,']),
+            # 아이리
+            'Irey': BaseItem('Irey', ['irie,']),
+            # 우편배달소녀
+            'Lettergirl': BaseItem('Lettergirl', ['lettergirl,']),
+            # 마리
+            'Mari': BaseItem('Mari', ['mari,']),
+            # 밀터
+            'Milter': BaseItem('Milter', ['milter,', 'milter_female,']),
+            # 나비걸
+            'Navy_girl': BaseItem('Navy_girl', ['navy_girl,']),
+            # 니노
+            'Nino': BaseItem('Nino', ['nino,']),
+            # 노아
+            'Noah': BaseItem('Noah', ['noah,']),
+            # 페르사
+            'Persa': BaseItem('Persa', ['persa,', 'fersa,']),
+            # 파더
+            'Father': BaseItem('Father', ['father,']),
+            # 렘
+            'Rem': BaseItem('Rem', ['rem,']),
+            # 린메이
+            'LinMei': BaseItem('LinMei', ['rinmei,', 'linmei,']),
+            # 린샤오
+            'LinXiao': BaseItem('LinXiao', ['rinshao,', 'rinshao_female,', 'linxiao,']),
+            # 루아이
+            'RU-i': BaseItem('RU-i', ['ru-i,']),
+            # 산토스
+            'Santos': BaseItem('Santos', ['santos,', 'santos_female,']),
+            # 시미즈
+            'Shimizu': BaseItem('Shimizu', ['shimizu,']),
+            # 단비
+            'Danbi': BaseItem('Danbi', ['space_girl,', 'space girl,', 'danbi,']),
+            # 드와이트
+            'Dwight': BaseItem('Dwight', ['dwight,', ]),
+            # Ohm
+            'Ohm': BaseItem('Ohm', ['ohm,', ]),
+            # Spitz
+            'Spitz': BaseItem('Spitz', ['spitz,', 'nsspitz,']),
+            # Campo
+            'Campo': BaseItem('Campo', ['campo,', 'nscampo,']),
+            # Vulkan
+            'Vulkan': BaseItem('Vulkan', ['vulkan,', 'nsvulkan,']),
+        }
+
+class OutfitManager(BaseManager):
+    def __init__(self):
+        super().__init__()
+        self.items = {
+            # 상의
+            'blouse': BaseItem('blouse', ['blouse,']),
+            'shirt': BaseItem('shirt', ['shirt,']),
+            't-shirt': BaseItem('t-shirt', ['t-shirt,']),
+            'cardigan': BaseItem('cardigan', ['cardigan,']),
+            'camisole': BaseItem('camisole', ['camisole,']),
+            'cape': BaseItem('cape', ['cape,']),
+            'cassock': BaseItem('cassock', ['cassock,']),
+            'coat': BaseItem('coat', ['coat,']),
+            'fur_coat': BaseItem('fur_coat', ['fur coat,', 'fur_coat,']),
+            'raincoat': BaseItem('raincoat', ['raincoat,']),
+            'jacket': BaseItem('jacket', ['jacket,']),
+            'blazer': BaseItem('blazer', ['blazer,']),
+            
+            # 전신복
+            'business_suit': BaseItem('business_suit', ['business suit,', 'business_suit,']),
+            'chinese_clothes': BaseItem('chinese_clothes', ['chinese clothes,', 'chinese_clothes,']),
+            'male_swimwear': BaseItem('male_swimwear', ['male swimwear,', 'male_swimwear,']),
+            'band_uniform': BaseItem('band_uniform', ['band uniform,', 'band_uniform,']),
+            'school_uniform': BaseItem('school_uniform', ['school uniform,', 'school_uniform,']),
+            'sportswear': BaseItem('sportswear', ['sportswear,']),
+            'tank_top': BaseItem('tank_top', ['tank top,', 'tank_top,']),
+            'gym_uniform': BaseItem('gym_uniform', ['gym uniform,', 'gym_uniform,']),
+            'sweater': BaseItem('sweater', ['sweater,']),
+            'crop_top': BaseItem('crop_top', ['crop top,', 'crop_top,']),
+            'tuxedo': BaseItem('tuxedo', ['tuxedo,']),
+            'winter_clothes': BaseItem('winter_clothes', ['winter clothes,', 'winter_clothes,']),
+            'military_uniform': BaseItem('military_uniform', ['military uniform,', 'military_uniform,', 'military,']),
+            
+            # 특수복
+            'apron': BaseItem('apron', ['apron,']),
+            'maid': BaseItem('maid', ['maid,']),
+            'cheerleader': BaseItem('cheerleader', ['cheerleader,']),
+            'nun': BaseItem('nun', ['nun,']),
+            
+            # 하의
+            'overalls': BaseItem('overalls', ['overalls,']),
+            'skirt': BaseItem('skirt', ['skirt,']),
+            'long_skirt': BaseItem('long_skirt', ['long skirt,', 'long_skirt,']),
+            'shorts': BaseItem('shorts', ['shorts,']),
+            'pants': BaseItem('pants', ['pants,']),
+            
+            # 스타일
+            'fashion': BaseItem('fashion', ['fashion,']),
+            'casual': BaseItem('casual', ['casual,']),
+            'dress': BaseItem('dress', ['dress,']),
+            'wedding': BaseItem('wedding', ['wedding,']),
+            'mechanical': BaseItem('mechanical', ['mechanical,']),
+            
+            # 수영복/댄스복
+            'bikini': BaseItem('bikini', ['bikini,']),
+            'swimsuit': BaseItem('swimsuit', ['swimsuit,']),
+            'ballet': BaseItem('ballet', ['ballet,']),
+            
+            # 전통의상
+            'japanese_clothes': BaseItem('japanese_clothes', ['japanese clothes,', 'japanese_clothes,']),
+            'korean_clothes': BaseItem('korean_clothes', ['korean clothes,', 'korean_clothes,']),
+            
+            # 코스튬
+            'animal_costume': BaseItem('animal_costume', ['animal costume,', 'animal_costume,']),
+            'santa_costume': BaseItem('santa_costume', ['santa costume,', 'santa_costume,']),
+            
+            # 속옷/스타킹
+            'bodysuit': BaseItem('bodysuit', ['bodysuit,']),
+            'bikesuit': BaseItem('bikesuit', ['bikesuit,']),
+            'jumpsuit': BaseItem('jumpsuit', ['jumpsuit,']),
+            'leotard': BaseItem('leotard', ['leotard,']),
+            'nightgown': BaseItem('nightgown', ['nightgown,']),
+            'kneehighs': BaseItem('kneehighs', ['kneehighs,']),
+            'pantyhose': BaseItem('pantyhose', ['pantyhose,']),
+            'thighhighs': BaseItem('thighhighs', ['thighhighs,']),
+            
+            # 신발
+            'shoes': BaseItem('shoes', ['shoes,']),
+            'boots': BaseItem('boots', ['boots,']),
+            'high_heels': BaseItem('high_heels', ['high heels,', 'high_heels,']),
+            'sandals': BaseItem('sandals', ['sandals,']),
+            'sneakers': BaseItem('sneakers', ['sneakers,']),
+            
+            # 액세서리
+            'headphones': BaseItem('headphones', ['headphones,']),
+            'mask': BaseItem('mask', ['mask,']),
+            'necktie': BaseItem('necktie', ['necktie,']),
+            'weapon': BaseItem('weapon', ['weapon,']),
+        }
+
+class EventManager(BaseManager):
+    def __init__(self):
+        super().__init__()
+        self.items = {
+            # 배경
+            'simple_background': BaseItem('simple_background', ['simple background,', 'simple_background,']),
+            
+            # 인물 수
+            'girl': BaseItem('girl', ['1girl,', 'girl,']),
+            'boy': BaseItem('boy', ['1boy,', 'boy,']),
+            
+            # 장소
+            'train': BaseItem('train', ['train,']),
+            'street': BaseItem('street', ['street,']),
+            'park': BaseItem('park', ['park,']),
+            'outdoors': BaseItem('outdoors', ['outdoors,']),
+            'ocean': BaseItem('ocean', ['ocean,']),
+            'indoors': BaseItem('indoors', ['indoors,']),
+            'cyberpunk': BaseItem('cyberpunk', ['cyberpunk,']),
+            'city': BaseItem('city', ['city,']),
+            'alley': BaseItem('alley', ['alley,']),
+            
+            # 계절/이벤트
+            'get-promoted' : BaseItem('get-promoted', ['get-promoted,']),
+            'autumn': BaseItem('autumn', ['autumn,']),
+            'christmas': BaseItem('christmas', ['christmas,']),
+            'flower': BaseItem('flower', ['flower,']),
+            'halloween': BaseItem('halloween', ['halloween,']),
+            'new-year': BaseItem('new-year', ['new-year,', 'new year,']),
+            'swimming': BaseItem('swimming', ['swimming,']),
+            'thanksgiving-day': BaseItem('thanksgiving-day', ['thanksgiving-day,']),
+            'valentine': BaseItem('valentine', ['valentine,']),
+            'winter-festival': BaseItem('winter-festival', ['winter-festival,']),
+            'winter-clothes': BaseItem('winter-clothes', ['winter-clothes,', 'winter clothes,']),
+            'volunteer-work': BaseItem('volunteer-work', ['volunteer-work,', 'volunteer work,']),
+            'crime-scene': BaseItem('crime-scenes', ['crime-scene,', 'crime scene,']),
+        }
